70dff501742393c9ea220fb1703c36f4023e2e40
spl1ce0 <spl1ce0root@gmail.com>
2026-03-08 21:43:26 +0000

    Fix runtime crash caused by TYPE_CHECKING import

--- cogs/games/__init__.py
+++ cogs/games/__init__.py
@@ -1,4 +1,3 @@
-
 from typing import TYPE_CHECKING
 import logging
 
@@ -8,7 +7,7 @@ logger = logging.getLogger(__name__)
 if TYPE_CHECKING:
     from bot import Spl1ceAI 
 
-async def setup(bot: Spl1ceAI) -> None:
+async def setup(bot: 'Spl1ceAI') -> None:
 
     from .cog import Games
 
