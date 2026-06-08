4cd53f395175eb1890ef89ec733ba0a24fcae2a3
spl1ce0 <spl1ceroot@gmail.com>
2026-06-08 17:00:05 +0100

    refactor: moved fakeban command to fun cog and removed troll cog

--- cogs/fun.py
+++ cogs/fun.py
@@ -6,8 +6,34 @@ import random
 import logging
 import tempfile
 import os
+from discord import ui
+from discord.ext.commands import MemberConverter
 
 logger = logging.getLogger(__name__)
+
+
+
+class FakeBanContainer(ui.Container):
+    
+    BANNED_EMOJI = "<a:sAI_banned:1478422889668542545>"
+
+    def __init__(self, user: discord.Member, reason: str):
+        super().__init__()
+        self.user = user
+        self.reason = reason
+        self._make_container()
+
+
+    def _make_container(self):
+        banDisplay = ui.TextDisplay(f"### {self.BANNED_EMOJI} {self.user.mention} has been banned.\n")
+        self.add_item(banDisplay)
+
+        reasonDisplay = ui.TextDisplay(f"- **Reason:** {self.reason}")
+        self.add_item(reasonDisplay)
+        
+        self.accent_color = discord.Color.red()
+
+
 
 class Fun(commands.Cog):
     def __init__(self, bot):
@@ -117,6 +143,26 @@ class Fun(commands.Cog):
             except Exception as e:
                 logger.error(f"Sealion command failed: {e}")
                 await ctx.reply(f"Something went wrong while fetching the sealion! 🦭\n`{e}`")
+
+
+    
+    @commands.hybrid_command(name="ban", aliases=[])
+    async def fakeban(self, ctx, member, *, reason):
+        """Sends a fake ban message."""
+
+        try:
+            user = await MemberConverter().convert(ctx, member)
+        except commands.MemberNotFound:  
+            await ctx.reply("User not found.\n-# Mention the user or provide their ID.", ephemeral=True)  
+            return
+        
+        ban_container = FakeBanContainer(user, reason)
+        
+        view = ui.LayoutView()
+        view.add_item(ban_container)
+
+        await ctx.reply(view=view)
+
 
 async def setup(bot):
     await bot.add_cog(Fun(bot))
